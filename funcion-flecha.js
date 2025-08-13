df93e844d00b4bbb305b305f764e360936218586
NewbieFer <diegoaguirregalvis@outlook.com>
2025-08-12 19:31:29 -0500

    aprendiendo a realizar la funcion flecha

---
+++ funcion-flecha.js
@@ -0,0 +1,11 @@
+function nombreDeLaFuncion(n1, n2) {
+  console.log("los numeros son:", n1, n2);
+}
+
+const variable = () => {};
+
+const ImprimirNombre = (nombre) => {
+  return "hola" + nombre;
+};
+resultado = ImprimirNombre("Diego");
+console.log(resultado);
