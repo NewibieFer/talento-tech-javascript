61e4a5423fc474c2a7038bb1b053a3ceac9cc67c
NewbieFer <diegoaguirregalvis@outlook.com>
2025-08-12 19:49:02 -0500

    adicion del DOM

--- funcion-flecha.js
+++ funcion-flecha.js
@@ -2,10 +2,14 @@ function nombreDeLaFuncion(n1, n2) {
   console.log("los numeros son:", n1, n2);
 }
 
+
 const variable = () => {};
 
+
 const ImprimirNombre = (nombre) => {
-  return "hola" + nombre;
+  return "Hola " + nombre;
 };
 resultado = ImprimirNombre("Diego");
 console.log(resultado);
+
+
